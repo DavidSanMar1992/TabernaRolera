55e3e6320e45777d430b46a49caac275f32a4783
Linky <david.s.tec.info@gmail.com>
2017-07-15 17:08:40 +0200

    Primer retoque de las vistas

--- TabernaRolera/TabernaRolera/App_Start/BundleConfig.cs
+++ TabernaRolera/TabernaRolera/App_Start/BundleConfig.cs
@@ -24,7 +24,6 @@ namespace TabernaRolera
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
